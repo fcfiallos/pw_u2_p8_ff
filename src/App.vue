54537bdbe974069506f24e291893e457039776a2
fcfiallos <fcfiallos@uce.edu.ec>
2025-05-14 18:31:10 -0500

    taller 12

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HolaMundoView></HolaMundoView>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HolaMundoView from "../src/views/HolaMundoView.vue";
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HolaMundoView,
+  },
+};
 </script>
 
 <style>
