94a8840d0fb655a77b140f09e06e0c81d05fe70b
fcfiallos <fcfiallos@uce.edu.ec>
2025-05-22 19:28:46 -0500

    taller 17: Cliente de apis

--- src/components/Pregunta.vue
+++ src/components/Pregunta.vue
@@ -1,36 +1,62 @@
 <template>
   <div class="container">
-    <img
-      src="https://yesno.wtf/assets/no/19-2062f4c91189b1f88a9e809c10a5b0f0.gif"
-      alt="No se pudo cargar"
-    />
+    <img v-if="image" :src="image" alt="No se pudo cargar" />
     <div class="container-2"></div>
     <div class="pregunta-container">
-      <input v-model="pregunta"
+      <input
+        v-model="pregunta"
         type="text"
         placeholder="Hazme una pregunta"
       /><!--una idea de que se debe poner en el input-->
       <p>Recuerda terminar con un signo de pregunta (?)</p>
-      <h2>{{ pregunta }}</h2>
-      <h1>{{respuesta}}</h1>
+      <div v-if="esValida">
+        <h2>{{ pregunta }}</h2>
+        <h1>{{ respuesta }}</h1>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+// en las llaves se pone los metodos que se quieren usar, por ello en los metodos se usa el export
+import { consultarRespuestaFachada } from "@/clients/YesNoClient.js";
 export default {
-    data(){
-        return{
-            pregunta: null, //pregunta 
-            respuesta: null, //api
-        }
+  data() {
+    return {
+      pregunta: null, //pregunta
+      respuesta: null, //api
+      image: null,
+      esValida: false,
+    };
+  },
+  //cuando no son method no hace falta poner async
+  watch: {
+    //permite observar los cambio de mi propiedad reactiva
+    pregunta(value, oldValue) {
+      this.esValida = false;
+      if (value.includes("?")) {
+        this.esValida = true;
+        // si el valor incluye el signo
+        console.log("valor actual: " + value);
+        console.log("valor anterior: " + oldValue);
+        //Es decir entro de aqui se debe consultar el api
+        this.consumirAPI();
+      }
     },
-    watch:{ //permite observar los cambio de mi propiedad reactiva 
-        pregunta(value, oldValue){
-            console.log("valor actual: "+ value);
-            console.log("valor anterior: "+ oldValue);
-        }
-    }
+  },
+  methods: {
+    async consumirAPI() {
+      this.respuesta = "Pensando......";
+      // es un metodo async por ende se necita que se quede esperando
+      const resp = await consultarRespuestaFachada();
+      console.log(resp);
+      console.log(resp.image);
+      console.log(resp.answer);
+      console.log(resp.forced); //esto se puede desestructurar
+      this.respuesta = resp.answer;
+      this.image = resp.image;
+    },
+  },
 };
 </script>
 
